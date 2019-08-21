95e3ba39c0e30302c500d3041e288bc1b1e31a60
Archy <raphael93@yandex.ru>
2019-08-21 14:31:25 +0300

    Edited "Task.java". Added setters, getters, fields and constructors.

--- RexNegotumOrganizerCore/src/com/rexnegotium/model/Task.java
+++ RexNegotumOrganizerCore/src/com/rexnegotium/model/Task.java
@@ -1,8 +1,72 @@
 package com.rexnegotium.model;
 
-public class Task {
+import java.util.Date;
 
-	// Возьми за основу класс User, посмотри на форму создания новой задачи в "макете" 
-	// и попробуй создать похожий класс задачи здесь
+public class Task {
+	
+	private int tid;
+    private String tname;
+    private String note;
+    private String context;
+    private Date begintime;
+    private Date endtime;
+    private boolean completed;
+    //private boolean repeatable; // РїРѕРІС‚РѕСЂРµРЅРёРµ Р·Р°РґР°РЅРёСЏ, 
+    
+    
+    public int getTid() {
+    	return tid;
+    }
+    public void setTid(int tid) {
+    	this.tid = tid;
+    }
+    
+    public String getTname() {
+    	return tname;
+    }    
+    public void setTname(String tname) {
+    	this.tname = tname;
+    }
+    
+    private String getContext() {
+		return context;
+	}
+	private void setContext(String context) {
+		this.context = context;
+	}
+	public String getNote() {
+    	return note;
+    }
+    public void setNote(String note) {
+    	this.note = note;
+    }
+	private Date getBegintime() {
+		return begintime;
+	}
+	private void setBegintime(Date begintime) {
+		this.begintime = begintime;
+	}
+	private Date getEndtime() {
+		return endtime;
+	}
+	private void setEndtime(Date endtime) {
+		this.endtime = endtime;
+	}
+	private boolean isCompleted() {
+		return completed;
+	}
+	private void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+	
+	/*private boolean isRepeatable() {
+		return repeatable;
+	}
+	private void setRepeatable(boolean repeatable) {
+		this.repeatable = repeatable; 
+	}*/
+    
+	// Р’РѕР·СЊРјРё Р·Р° РѕСЃРЅРѕРІСѓ РєР»Р°СЃСЃ User, РїРѕСЃРјРѕС‚СЂРё РЅР° С„РѕСЂРјСѓ СЃРѕР·РґР°РЅРёСЏ РЅРѕРІРѕР№ Р·Р°РґР°С‡Рё РІ "РјР°РєРµС‚Рµ" 
+	// Рё РїРѕРїСЂРѕР±СѓР№ СЃРѕР·РґР°С‚СЊ РїРѕС…РѕР¶РёР№ РєР»Р°СЃСЃ Р·Р°РґР°С‡Рё Р·РґРµСЃСЊ
 	
 }
